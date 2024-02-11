5f9453636b02a55e05808c214b57bd9d206510ad
Jeff Linse <jeff@jefflinse.com>
2024-02-10 23:25:41 -0800

    Add comments

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -13,6 +13,82 @@ import (
 )
 
 func main() {
+	ctx := context.Background()
+	configureLogging()
+
+	// 1. Create an Event Store to store events.
+	eventStore := &memoryes.EventStore{}
+
+	// 2. Create an AggregateStore store aggregates.
+	aggregateStore := &continuum.AggregateStore{
+		Events: eventStore,
+	}
+
+	// 3. Define an aggregate type.
+	accountAggregateType, err := continuum.NewAggregateType("account",
+		func() continuum.AggregateData {
+			return NewAccount()
+		},
+		continuum.WithAggregateIDFactory(func() continuum.Identifier {
+			return continuum.UUID(uuid.New())
+		}),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// 4. Create an aggregate instance.
+	aggregate := accountAggregateType.NewAggregate(nil)
+
+	if err := aggregate.Append(
+		&UserCreatedEvent{Username: "jdoe"},
+		&BalanceChangedEvent{Amount: 100},
+		&UserCreatedEvent{Username: "bschmoe"},
+		&BalanceChangedEvent{Amount: -14},
+		&UserDeletedEvent{Username: "jdoe"},
+	); err != nil {
+		panic(err)
+	}
+
+	// save the aggregate
+	if err := aggregateStore.Save(ctx, aggregate); err != nil {
+		panic(err)
+	}
+
+	// load the aggregate
+	loadedAggregate, err := aggregateStore.Load(ctx, aggregate.ID())
+	if err != nil {
+		panic(err)
+	}
+
+	// newEvents, err := aggregate.Data.Diff(&Account{
+	// 	ID:      "123",
+	// 	Users:   []string{"bschmoe", "rlowe"},
+	// 	Balance: 80,
+	// })
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// if err := aggregate.Append(newEvents...); err != nil {
+	// 	panic(err)
+	// }
+
+	// if err := aggregateStore.Save(ctx, aggregate); err != nil {
+	// 	panic(err)
+	// }
+
+	// aggregate, err = aggregateStore.Load(ctx, continuum.StringID("123"))
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// get the aggregate data
+	account := loadedAggregate.Data().(*Account)
+	fmt.Println(account)
+}
+
+func configureLogging() {
 	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
 		Level: slog.LevelDebug,
 		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
@@ -33,74 +109,4 @@ func main() {
 			return a
 		},
 	})))
-
-	ctx := context.Background()
-
-	eventStore := &memoryes.EventStore{}
-
-	aggregateCollection := &continuum.AggregateStore{
-		Events: eventStore,
-	}
-
-	// define an aggregate type
-	accountAggregateType, err := continuum.NewAggregateType("account",
-		func() continuum.AggregateData {
-			return NewAccount()
-		},
-		continuum.WithAggregateIDFactory(func() continuum.Identifier {
-			return continuum.UUID(uuid.New())
-		}),
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	// create an aggregate instance
-	aggregate := accountAggregateType.NewAggregate(nil)
-
-	if err := aggregate.Append(
-		&UserCreatedEvent{Username: "jdoe"},
-		&BalanceChangedEvent{Amount: 100},
-		&UserCreatedEvent{Username: "bschmoe"},
-		&BalanceChangedEvent{Amount: -14},
-		&UserDeletedEvent{Username: "jdoe"},
-	); err != nil {
-		panic(err)
-	}
-
-	// save the aggregate
-	if err := aggregateCollection.Save(ctx, aggregate); err != nil {
-		panic(err)
-	}
-
-	// load the aggregate
-	loadedAggregate, err := aggregateCollection.Load(ctx, aggregate.ID())
-	if err != nil {
-		panic(err)
-	}
-
-	// newEvents, err := aggregate.Data.Diff(&Account{
-	// 	ID:      "123",
-	// 	Users:   []string{"bschmoe", "rlowe"},
-	// 	Balance: 80,
-	// })
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// if err := aggregate.Append(newEvents...); err != nil {
-	// 	panic(err)
-	// }
-
-	// if err := aggregateCollection.Save(ctx, aggregate); err != nil {
-	// 	panic(err)
-	// }
-
-	// aggregate, err = aggregateCollection.Load(ctx, continuum.StringID("123"))
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	account := loadedAggregate.Data().(*Account)
-	fmt.Println(account)
 }
