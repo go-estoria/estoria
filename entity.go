e7109f846c9360500f27f9292a673d051ec5fde9
Jeff Linse <jeff@jefflinse.com>
2024-02-11 23:24:55 -0800

    update comments

--- entity.go
+++ entity.go
@@ -2,16 +2,17 @@ package continuum
 
 import "context"
 
-// Entity is anything whose state can be constructed by applying a series of events.
+// An Entity is anything whose state can be constructed by applying a series of events.
 type Entity interface {
 	ApplyEvent(ctx context.Context, eventData EventData) error
 }
 
-// DiffableEntity is aggregate data that can be diffed against another aggregate data
+// A DiffableEntity is aggregate data that can be diffed against another aggregate data
 // of the same type to produce a series of events that represent the state changes between the two.
 type DiffableEntity interface {
 	Entity
 	Diff(newer DiffableEntity) ([]any, error)
 }
 
+// An EntityFactory is a function that creates a new instance of an entity.
 type EntityFactory[E Entity] func() E
