d13b3aba042baf8d70c75239beaa0f5ba33c1add
Jeff Linse <jeff@jefflinse.com>
2024-02-27 18:29:31 -0800

    Add CreateStream() for in-memory event store

--- eventstore/memory/event_store.go
+++ eventstore/memory/event_store.go
@@ -14,10 +14,26 @@ type EventStore struct {
 	mu sync.RWMutex
 }
 
+func (c *EventStore) CreateStream(ctx context.Context, aggregateID estoria.TypedID) (estoria.EventStream, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if _, ok := c.Events[aggregateID.String()]; ok {
+		return nil, fmt.Errorf("stream already exists: %s", aggregateID)
+	}
+
+	c.Events[aggregateID.String()] = []estoria.Event{}
+	return &EventStream{
+		id:     aggregateID.ID,
+		cursor: 0,
+		events: c.Events[aggregateID.String()],
+	}, nil
+}
+
 func (s *EventStore) FindStream(ctx context.Context, aggregateID estoria.TypedID) (estoria.EventStream, error) {
 	events, ok := s.Events[aggregateID.String()]
 	if !ok {
-		return nil, nil
+		return nil, estoria.ErrStreamNotFound
 	}
 
 	return &EventStream{
