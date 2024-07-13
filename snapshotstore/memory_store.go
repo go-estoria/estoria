8833b4a176533e65f16da2ca5f44fe3db4555fb3
Jeff Linse <jeff@jefflinse.com>
2024-07-13 11:25:17 -0700

    Fix snapshot retention

--- snapshotstore/memory_store.go
+++ snapshotstore/memory_store.go
@@ -19,7 +19,7 @@ func NewMemorySnapshotStore() *MemorySnapshotStore {
 	return &MemorySnapshotStore{
 		snapshots: map[typeid.UUID][]*AggregateSnapshot{},
 		marshaler: estoria.JSONMarshaler[AggregateSnapshot]{},
-		retention: MaxSnapshotsRetentionPolicy{N: 0},
+		retention: MaxSnapshotsRetentionPolicy{N: 1},
 	}
 }
 
@@ -28,20 +28,25 @@ func (s *MemorySnapshotStore) ReadSnapshot(ctx context.Context, aggregateID type
 
 	snapshots, ok := s.snapshots[aggregateID]
 	if !ok || len(snapshots) == 0 {
+		slog.Debug("no snapshots found", "aggregate_id", aggregateID)
 		return nil, nil
 	}
 
 	if opts.MaxVersion > 0 {
 		for i := len(snapshots) - 1; i >= 0; i-- {
-			if snapshots[i].AggregateVersion <= opts.MaxVersion {
+			if snap := snapshots[i]; snap.AggregateVersion <= opts.MaxVersion {
+				slog.Debug("found snapshot", "aggregate_id", snap.AggregateID, "aggregate_version", snap.AggregateVersion)
 				return snapshots[i], nil
 			}
 		}
 
+		slog.Debug("no snapshot found within version range", "aggregate_id", aggregateID, "max_version", opts.MaxVersion)
 		return nil, nil
 	}
 
-	return snapshots[len(snapshots)-1], nil
+	snap := snapshots[len(snapshots)-1]
+	slog.Debug("found snapshot", "aggregate_id", snap.AggregateID, "aggregate_version", snap.AggregateVersion)
+	return snap, nil
 }
 
 func (s *MemorySnapshotStore) WriteSnapshot(ctx context.Context, snap *AggregateSnapshot) error {
@@ -64,6 +69,18 @@ func (s *MemorySnapshotStore) WriteSnapshot(ctx context.Context, snap *Aggregate
 	}
 
 	s.snapshots[snap.AggregateID] = append(s.snapshots[snap.AggregateID], snap)
+
+	retained := []*AggregateSnapshot{}
+	for i, snap := range s.snapshots[snap.AggregateID] {
+		if !s.retention.ShouldRetain(snap, int64(i), int64(len(s.snapshots[snap.AggregateID]))) {
+			slog.Debug("deleting snapshot per retention policy", "aggregate_id", snap.AggregateID, "aggregate_version", snap.AggregateVersion)
+			continue
+		}
+
+		retained = append(retained, snap)
+	}
+
+	s.snapshots[snap.AggregateID] = retained
 
 	slog.Debug("wrote snapshot", "aggregate_id", snap.AggregateID, "aggregate_version", snap.AggregateVersion)
 
