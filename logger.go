4f1c0bf5c81c198840d96fbd047ee34b3a91f1ab
Jeff Linse <jeff@jefflinse.com>
2024-09-11 20:21:03 -0700

    Comments to disable some linters for logging code

--- logger.go
+++ logger.go
@@ -2,8 +2,10 @@ package estoria
 
 import "log/slog"
 
+//nolint:gochecknoglobals // The logger is a global component
 var logger Logger
 
+//nolint:gochecknoinits // The logger is a global component
 func init() {
 	logger = DefaultLogger()
 }
@@ -15,10 +17,12 @@ type Logger interface {
 	Error(msg string, args ...any)
 }
 
+//nolint:ireturn // Deliberately an interface
 func DefaultLogger() Logger {
 	return slog.Default()
 }
 
+//nolint:ireturn // Deliberately an interface
 func GetLogger() Logger {
 	return logger
 }
