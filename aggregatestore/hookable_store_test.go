3efa83c33dc4c3a5b7adfe3c4eaee6088dba94e1
Jeff Linse <jeff@jefflinse.com>
2024-09-14 17:15:53 -0700

    Add UTs for hookable store Hydrate

--- aggregatestore/hookable_store_test.go
+++ aggregatestore/hookable_store_test.go
@@ -395,316 +395,78 @@ func TestHookableStore_Load(t *testing.T) {
 	}
 }
 
-// func TestHookableStore_Hydrate(t *testing.T) {
-// 	t.Parallel()
+func TestHookableStore_Hydrate(t *testing.T) {
+	t.Parallel()
 
-// 	aggregateID := typeid.Must(typeid.NewUUID("mockentity")).(typeid.UUID)
+	aggregateID := typeid.Must(typeid.NewUUID("mockentity")).(typeid.UUID)
 
-// 	for _, tt := range []struct {
-// 		name               string
-// 		haveInner          aggregatestore.Store[*mockEntity]
-// 		havePrecreateHooks []aggregatestore.PrecreateHook
-// 		havePreloadHooks   []aggregatestore.PreloadHook
-// 		haveHooks          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]
-// 		haveAggregate      *aggregatestore.Aggregate[*mockEntity]
-// 		haveOpts           aggregatestore.HydrateOptions
-// 		wantAggregate      *aggregatestore.Aggregate[*mockEntity]
-// 		wantErr            error
-// 	}{
-// 		{
-// 			name: "hydrates an aggregate to a snapshot version",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				NewFn: func(id uuid.UUID) (*aggregatestore.Aggregate[*mockEntity], error) {
-// 					agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 					agg.State().SetEntityAtVersion(&mockEntity{ID: typeid.FromUUID("mockentity", id)}, 0)
-// 					return agg, nil
-// 				},
-// 				HydrateFn: func(_ context.Context, _ *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.HydrateOptions) error {
-// 					return nil
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 0)
-// 				return agg
-// 			}(),
-// 			haveOpts: aggregatestore.HydrateOptions{
-// 				ToVersion: 42,
-// 			},
-// 			wantAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
-// 				return agg
-// 			}(),
-// 		},
-// 		{
-// 			name: "hydrates an aggregate to a snapshot version then further hydrates it using the inner store",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				NewFn: func(id uuid.UUID) (*aggregatestore.Aggregate[*mockEntity], error) {
-// 					agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 					agg.State().SetEntityAtVersion(&mockEntity{ID: typeid.FromUUID("mockentity", id)}, 0)
-// 					return agg, nil
-// 				},
-// 				HydrateFn: func(_ context.Context, aggregate *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.HydrateOptions) error {
-// 					aggregate.State().SetEntityAtVersion(aggregate.Entity(), aggregate.Version()+3)
-// 					return nil
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 0)
-// 				return agg
-// 			}(),
-// 			wantAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 45)
-// 				return agg
-// 			}(),
-// 		},
-// 		// {
-// 		// 	name: "passes the correct MaxVersion read snapshot option",
-// 		// },
-// 		{
-// 			name: "falls back to hydrating using the inner store when already at the target version",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				NewFn: func(id uuid.UUID) (*aggregatestore.Aggregate[*mockEntity], error) {
-// 					agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 					agg.State().SetEntityAtVersion(&mockEntity{ID: typeid.FromUUID("mockentity", id)}, 0)
-// 					return agg, nil
-// 				},
-// 				HydrateFn: func(_ context.Context, _ *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.HydrateOptions) error {
-// 					return nil
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
-// 				return agg
-// 			}(),
-// 			haveOpts: aggregatestore.HydrateOptions{
-// 				ToVersion: 42,
-// 			},
-// 			wantAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
-// 				return agg
-// 			}(),
-// 		},
-// 		{
-// 			name: "falls back to hydrating using the inner store when target version is less than current version",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				NewFn: func(id uuid.UUID) (*aggregatestore.Aggregate[*mockEntity], error) {
-// 					agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 					agg.State().SetEntityAtVersion(&mockEntity{ID: typeid.FromUUID("mockentity", id)}, 0)
-// 					return agg, nil
-// 				},
-// 				HydrateFn: func(_ context.Context, _ *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.HydrateOptions) error {
-// 					return nil
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
-// 				return agg
-// 			}(),
-// 			haveOpts: aggregatestore.HydrateOptions{
-// 				ToVersion: 37,
-// 			},
-// 			wantAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
-// 				return agg
-// 			}(),
-// 		},
-// 		{
-// 			name: "falls back to hydrating using the inner store when reading a snapshot fails",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				NewFn: func(id uuid.UUID) (*aggregatestore.Aggregate[*mockEntity], error) {
-// 					agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 					agg.State().SetEntityAtVersion(&mockEntity{ID: typeid.FromUUID("mockentity", id)}, 0)
-// 					return agg, nil
-// 				},
-// 				HydrateFn: func(_ context.Context, aggregate *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.HydrateOptions) error {
-// 					aggregate.State().SetEntityAtVersion(aggregate.Entity(), aggregate.Version()+3)
-// 					return nil
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
-// 				return agg
-// 			}(),
-// 			wantAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 45)
-// 				return agg
-// 			}(),
-// 		},
-// 		{
-// 			name: "falls back to hydrating using the inner store when no snapshot is available",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				NewFn: func(id uuid.UUID) (*aggregatestore.Aggregate[*mockEntity], error) {
-// 					agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 					agg.State().SetEntityAtVersion(&mockEntity{ID: typeid.FromUUID("mockentity", id)}, 0)
-// 					return agg, nil
-// 				},
-// 				HydrateFn: func(_ context.Context, aggregate *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.HydrateOptions) error {
-// 					aggregate.State().SetEntityAtVersion(aggregate.Entity(), aggregate.Version()+3)
-// 					return nil
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
-// 				return agg
-// 			}(),
-// 			wantAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 45)
-// 				return agg
-// 			}(),
-// 		},
-// 		{
-// 			name: "falls back to hydrating using the inner store the snapshot cannot be unmarshaled",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				NewFn: func(id uuid.UUID) (*aggregatestore.Aggregate[*mockEntity], error) {
-// 					agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 					agg.State().SetEntityAtVersion(&mockEntity{ID: typeid.FromUUID("mockentity", id)}, 0)
-// 					return agg, nil
-// 				},
-// 				HydrateFn: func(_ context.Context, aggregate *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.HydrateOptions) error {
-// 					aggregate.State().SetEntityAtVersion(aggregate.Entity(), aggregate.Version()+3)
-// 					return nil
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
-// 				return agg
-// 			}(),
-// 			wantAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 45)
-// 				return agg
-// 			}(),
-// 		},
-// 		{
-// 			name: "returns an error when the aggregate is nil",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				NewFn: func(id uuid.UUID) (*aggregatestore.Aggregate[*mockEntity], error) {
-// 					agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 					agg.State().SetEntityAtVersion(&mockEntity{ID: typeid.FromUUID("mockentity", id)}, 0)
-// 					return agg, nil
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate:      nil,
-// 			wantErr:            aggregatestore.HydrateAggregateError{Err: aggregatestore.ErrNilAggregate},
-// 		},
-// 		{
-// 			name: "returns an error when the target version is invalid",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				NewFn: func(id uuid.UUID) (*aggregatestore.Aggregate[*mockEntity], error) {
-// 					agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 					agg.State().SetEntityAtVersion(&mockEntity{ID: typeid.FromUUID("mockentity", id)}, 0)
-// 					return agg, nil
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
-// 				return agg
-// 			}(),
-// 			haveOpts: aggregatestore.HydrateOptions{
-// 				ToVersion: -1,
-// 			},
-// 			wantErr: aggregatestore.HydrateAggregateError{Err: errors.New("invalid target version")},
-// 		},
-// 		{
-// 			name: "returns an error when the snapshot stoe reader is nil",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				NewFn: func(id uuid.UUID) (*aggregatestore.Aggregate[*mockEntity], error) {
-// 					agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 					agg.State().SetEntityAtVersion(&mockEntity{ID: typeid.FromUUID("mockentity", id)}, 0)
-// 					return agg, nil
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
-// 				return agg
-// 			}(),
-// 			wantErr: aggregatestore.HydrateAggregateError{Err: errors.New("snapshot store has no snapshot reader")},
-// 		},
-// 	} {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			t.Parallel()
+	for _, tt := range []struct {
+		name          string
+		haveInner     aggregatestore.Store[*mockEntity]
+		haveAggregate *aggregatestore.Aggregate[*mockEntity]
+		haveOpts      aggregatestore.HydrateOptions
+		wantAggregate *aggregatestore.Aggregate[*mockEntity]
+		wantErr       error
+	}{
+		{
+			name: "hydrates an aggregate using the inner store",
+			haveInner: &mockAggregateStore[*mockEntity]{
+				HydrateFn: func(_ context.Context, agg *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.HydrateOptions) error {
+					agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
+					return nil
+				},
+			},
+			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
+				agg := &aggregatestore.Aggregate[*mockEntity]{}
+				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 0)
+				return agg
+			}(),
+			wantAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
+				agg := &aggregatestore.Aggregate[*mockEntity]{}
+				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
+				return agg
+			}(),
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 
-// 			store, err := aggregatestore.NewHookableStore(
-// 				tt.haveInner,
-// 			)
-// 			if err != nil {
-// 				t.Fatalf("unexpected error creating store: %v", err)
-// 			} else if store == nil {
-// 				t.Fatal("unexpected nil store")
-// 			}
+			store, err := aggregatestore.NewHookableStore(
+				tt.haveInner,
+			)
+			if err != nil {
+				t.Fatalf("unexpected error creating store: %v", err)
+			} else if store == nil {
+				t.Fatal("unexpected nil store")
+			}
 
-// 			gotAggregate := tt.haveAggregate
-// 			gotErr := store.Hydrate(context.Background(), gotAggregate, tt.haveOpts)
+			gotAggregate := tt.haveAggregate
+			gotErr := store.Hydrate(context.Background(), gotAggregate, tt.haveOpts)
 
-// 			if tt.wantErr != nil {
-// 				if gotErr == nil || gotErr.Error() != tt.wantErr.Error() {
-// 					t.Errorf("want error: %v, got: %v", tt.wantErr, gotErr)
-// 				}
-// 				return
-// 			}
+			if tt.wantErr != nil {
+				if gotErr == nil || gotErr.Error() != tt.wantErr.Error() {
+					t.Errorf("want error: %v, got: %v", tt.wantErr, gotErr)
+				}
+				return
+			}
 
-// 			if gotErr != nil {
-// 				t.Errorf("unexpected error: %v", gotErr)
-// 			} else if gotAggregate == nil {
-// 				t.Errorf("unexpected nil aggregate")
-// 			}
+			if gotErr != nil {
+				t.Errorf("unexpected error: %v", gotErr)
+			} else if gotAggregate == nil {
+				t.Errorf("unexpected nil aggregate")
+			}
 
-// 			// aggregate has the correct ID
-// 			if gotAggregate.ID().String() != typeid.FromUUID("mockentity", aggregateID.UUID()).String() {
-// 				t.Errorf("want aggregate ID %s, got %s", typeid.FromUUID("mockentity", aggregateID.UUID()), gotAggregate.ID())
-// 			}
-// 			// aggregate has the correct version
-// 			if gotAggregate.Version() != tt.wantAggregate.Version() {
-// 				t.Errorf("want aggregate version %d, got %d", tt.wantAggregate.Version(), gotAggregate.Version())
-// 			}
-// 		})
-// 	}
-// }
+			// aggregate has the correct ID
+			if gotAggregate.ID().String() != typeid.FromUUID("mockentity", aggregateID.UUID()).String() {
+				t.Errorf("want aggregate ID %s, got %s", typeid.FromUUID("mockentity", aggregateID.UUID()), gotAggregate.ID())
+			}
+			// aggregate has the correct version
+			if gotAggregate.Version() != tt.wantAggregate.Version() {
+				t.Errorf("want aggregate version %d, got %d", tt.wantAggregate.Version(), gotAggregate.Version())
+			}
+		})
+	}
+}
 
 // func TestHookableStore_Save(t *testing.T) {
 // 	t.Parallel()
