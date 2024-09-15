e034db8e05ed2fcb65998fdc0b8b5294f0233869
Jeff Linse <jeff@jefflinse.com>
2024-09-14 17:21:31 -0700

    Add UTs for hookable store Save

--- aggregatestore/hookable_store_test.go
+++ aggregatestore/hookable_store_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/go-estoria/estoria"
 	"github.com/go-estoria/estoria/aggregatestore"
 	"github.com/go-estoria/estoria/typeid"
 	"github.com/gofrs/uuid/v5"
@@ -468,205 +469,210 @@ func TestHookableStore_Hydrate(t *testing.T) {
 	}
 }
 
-// func TestHookableStore_Save(t *testing.T) {
-// 	t.Parallel()
+func TestHookableStore_Save(t *testing.T) {
+	t.Parallel()
 
-// 	aggregateID := typeid.Must(typeid.NewUUID("mockentity")).(typeid.UUID)
+	aggregateID := typeid.Must(typeid.NewUUID("mockentity")).(typeid.UUID)
 
-// 	for _, tt := range []struct {
-// 		name               string
-// 		haveInner          aggregatestore.Store[*mockEntity]
-// 		havePrecreateHooks []aggregatestore.PrecreateHook
-// 		havePreloadHooks   []aggregatestore.PreloadHook
-// 		haveHooks          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]
-// 		haveAggregate      *aggregatestore.Aggregate[*mockEntity]
-// 		haveOpts           aggregatestore.SaveOptions
-// 		wantAggregate      *aggregatestore.Aggregate[*mockEntity]
-// 		wantErr            error
-// 	}{
-// 		{
-// 			name: "saves an aggregate using the inner store and creates no snapshot if the policy does not require it",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				SaveFn: func(_ context.Context, aggregate *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.SaveOptions) error {
-// 					aggregate.State().WillApply(&aggregatestore.AggregateEvent{
-// 						EntityEvent: &mockEntityEventA{},
-// 					})
-// 					return nil
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{
-// 					ID:         aggregateID,
-// 					eventTypes: []estoria.EntityEvent{mockEntityEventA{}},
-// 				}, 42)
-// 				return agg
-// 			}(),
-// 			wantAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 43)
-// 				return agg
-// 			}(),
-// 		},
-// 		{
-// 			name: "saves an aggregate using the inner store and creates no snapshot if the snapshot fails to marshal",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				SaveFn: func(_ context.Context, aggregate *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.SaveOptions) error {
-// 					aggregate.State().WillApply(&aggregatestore.AggregateEvent{
-// 						EntityEvent: &mockEntityEventA{},
-// 					})
-// 					return nil
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
-// 				return agg
-// 			}(),
-// 			wantAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 43)
-// 				return agg
-// 			}(),
-// 		},
-// 		{
-// 			name: "saves an aggregate using the inner store and creates no snapshot if the snappshot writer fails to write",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				SaveFn: func(_ context.Context, aggregate *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.SaveOptions) error {
-// 					aggregate.State().WillApply(&aggregatestore.AggregateEvent{
-// 						EntityEvent: &mockEntityEventA{},
-// 					})
-// 					return nil
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
-// 				return agg
-// 			}(),
-// 			wantAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 43)
-// 				return agg
-// 			}(),
-// 		},
-// 		{
-// 			name: "saves an aggregate using the inner store and creates a snapshot if the policy requires it",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				SaveFn: func(_ context.Context, aggregate *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.SaveOptions) error {
-// 					aggregate.State().WillApply(&aggregatestore.AggregateEvent{
-// 						EntityEvent: &mockEntityEventA{},
-// 					})
-// 					return nil
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
-// 				return agg
-// 			}(),
-// 			wantAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 43)
-// 				return agg
-// 			}(),
-// 		},
-// 		{
-// 			name:               "returns an error when the aggregate is nil",
-// 			haveInner:          &mockAggregateStore[*mockEntity]{},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate:      nil,
-// 			wantErr:            aggregatestore.SaveAggregateError{Err: aggregatestore.ErrNilAggregate},
-// 		},
-// 		{
-// 			name: "returns an error when the inner store returns an error",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				SaveFn: func(_ context.Context, _ *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.SaveOptions) error {
-// 					return errors.New("mock error")
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 42)
-// 				return agg
-// 			}(),
-// 			wantErr: aggregatestore.SaveAggregateError{Err: errors.New("saving aggregate using inner store: mock error")},
-// 		},
-// 		{
-// 			name: "returns an error when encountering an unexpected error applying an event",
-// 			haveInner: &mockAggregateStore[*mockEntity]{
-// 				SaveFn: func(_ context.Context, aggregate *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.SaveOptions) error {
-// 					aggregate.State().WillApply(&aggregatestore.AggregateEvent{
-// 						EntityEvent: &mockEntityEventA{},
-// 					})
-// 					return nil
-// 				},
-// 			},
-// 			havePrecreateHooks: []aggregatestore.PrecreateHook{},
-// 			havePreloadHooks:   []aggregatestore.PreloadHook{},
-// 			haveHooks:          map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
-// 			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
-// 				agg := &aggregatestore.Aggregate[*mockEntity]{}
-// 				agg.State().SetEntityAtVersion(&mockEntity{
-// 					ID:            aggregateID,
-// 					applyEventErr: errors.New("mock error"),
-// 				}, 42)
-// 				return agg
-// 			}(),
-// 			wantErr: aggregatestore.SaveAggregateError{Err: errors.New("applying next aggregate event: applying event: mock error")},
-// 		},
-// 	} {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			t.Parallel()
+	for _, tt := range []struct {
+		name          string
+		haveInner     aggregatestore.Store[*mockEntity]
+		haveHooks     map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]
+		haveAggregate *aggregatestore.Aggregate[*mockEntity]
+		haveOpts      aggregatestore.SaveOptions
+		wantAggregate *aggregatestore.Aggregate[*mockEntity]
+		wantErr       error
+	}{
+		{
+			name: "saves an aggregate using the inner store when no hooks are provided",
+			haveInner: &mockAggregateStore[*mockEntity]{
+				SaveFn: func(_ context.Context, aggregate *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.SaveOptions) error {
+					aggregate.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, aggregate.Version()+1)
+					return nil
+				},
+			},
+			haveHooks: map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{},
+			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
+				agg := &aggregatestore.Aggregate[*mockEntity]{}
+				agg.State().SetEntityAtVersion(&mockEntity{
+					ID:         aggregateID,
+					eventTypes: []estoria.EntityEvent{mockEntityEventA{}},
+				}, 42)
+				return agg
+			}(),
+			wantAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
+				agg := &aggregatestore.Aggregate[*mockEntity]{}
+				agg.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, 43)
+				return agg
+			}(),
+		},
+		{
+			name: "saves an aggregate using the inner store when a single pre-save hook is provided",
+			haveInner: &mockAggregateStore[*mockEntity]{
+				SaveFn: func(_ context.Context, aggregate *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.SaveOptions) error {
+					aggregate.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, aggregate.Version()+1)
+					return nil
+				},
+			},
+			haveHooks: map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{
+				aggregatestore.BeforeSave: {
+					func(_ context.Context, _ *aggregatestore.Aggregate[*mockEntity]) error {
+						return errors.New("mock error")
+					},
+				},
+			},
+			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
+				agg := &aggregatestore.Aggregate[*mockEntity]{}
+				agg.State().SetEntityAtVersion(&mockEntity{
+					ID:         aggregateID,
+					eventTypes: []estoria.EntityEvent{mockEntityEventA{}},
+				}, 42)
+				return agg
+			}(),
+			wantErr: errors.New("pre-save hook: mock error"),
+		},
+		{
+			name: "saves an aggregate using the inner store when multiple pre-save hooks are provided",
+			haveInner: &mockAggregateStore[*mockEntity]{
+				SaveFn: func(_ context.Context, aggregate *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.SaveOptions) error {
+					aggregate.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, aggregate.Version()+1)
+					return nil
+				},
+			},
+			haveHooks: map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{
+				aggregatestore.BeforeSave: {
+					func(_ context.Context, _ *aggregatestore.Aggregate[*mockEntity]) error {
+						return nil
+					},
+					func(_ context.Context, _ *aggregatestore.Aggregate[*mockEntity]) error {
+						return nil
+					},
+					func(_ context.Context, _ *aggregatestore.Aggregate[*mockEntity]) error {
+						return errors.New("mock error")
+					},
+				},
+			},
+			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
+				agg := &aggregatestore.Aggregate[*mockEntity]{}
+				agg.State().SetEntityAtVersion(&mockEntity{
+					ID:         aggregateID,
+					eventTypes: []estoria.EntityEvent{mockEntityEventA{}},
+				}, 42)
+				return agg
+			}(),
+			wantErr: errors.New("pre-save hook: mock error"),
+		},
+		{
+			name: "saves an aggregate using the inner store when a single post-save hook is provided",
+			haveInner: &mockAggregateStore[*mockEntity]{
+				SaveFn: func(_ context.Context, aggregate *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.SaveOptions) error {
+					aggregate.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, aggregate.Version()+1)
+					return nil
+				},
+			},
+			haveHooks: map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{
+				aggregatestore.AfterSave: {
+					func(_ context.Context, _ *aggregatestore.Aggregate[*mockEntity]) error {
+						return errors.New("mock error")
+					},
+				},
+			},
+			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
+				agg := &aggregatestore.Aggregate[*mockEntity]{}
+				agg.State().SetEntityAtVersion(&mockEntity{
+					ID:         aggregateID,
+					eventTypes: []estoria.EntityEvent{mockEntityEventA{}},
+				}, 42)
+				return agg
+			}(),
+			wantErr: errors.New("post-save hook: mock error"),
+		},
+		{
+			name: "saves an aggregate using the inner store when multiple post-save hooks are provided",
+			haveInner: &mockAggregateStore[*mockEntity]{
+				SaveFn: func(_ context.Context, aggregate *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.SaveOptions) error {
+					aggregate.State().SetEntityAtVersion(&mockEntity{ID: aggregateID}, aggregate.Version()+1)
+					return nil
+				},
+			},
+			haveHooks: map[aggregatestore.HookStage][]aggregatestore.Hook[*mockEntity]{
+				aggregatestore.AfterSave: {
+					func(_ context.Context, _ *aggregatestore.Aggregate[*mockEntity]) error {
+						return nil
+					},
+					func(_ context.Context, _ *aggregatestore.Aggregate[*mockEntity]) error {
+						return nil
+					},
+					func(_ context.Context, _ *aggregatestore.Aggregate[*mockEntity]) error {
+						return errors.New("mock error")
+					},
+				},
+			},
+			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
+				agg := &aggregatestore.Aggregate[*mockEntity]{}
+				agg.State().SetEntityAtVersion(&mockEntity{
+					ID:         aggregateID,
+					eventTypes: []estoria.EntityEvent{mockEntityEventA{}},
+				}, 42)
+				return agg
+			}(),
+			wantErr: errors.New("post-save hook: mock error"),
+		},
+		{
+			name: "returns an error when the inner store returns an error",
+			haveInner: &mockAggregateStore[*mockEntity]{
+				SaveFn: func(_ context.Context, _ *aggregatestore.Aggregate[*mockEntity], _ aggregatestore.SaveOptions) error {
+					return errors.New("mock error")
+				},
+			},
+			haveAggregate: func() *aggregatestore.Aggregate[*mockEntity] {
+				agg := &aggregatestore.Aggregate[*mockEntity]{}
+				agg.State().SetEntityAtVersion(&mockEntity{
+					ID:         aggregateID,
+					eventTypes: []estoria.EntityEvent{mockEntityEventA{}},
+				}, 42)
+				return agg
+			}(),
+			wantErr: errors.New("saving aggregate using inner store: mock error"),
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 
-// 			store, err := aggregatestore.NewHookableStore(tt.haveInner)
-// 			if err != nil {
-// 				t.Fatalf("unexpected error creating store: %v", err)
-// 			} else if store == nil {
-// 				t.Fatal("unexpected nil store")
-// 			}
+			store, err := aggregatestore.NewHookableStore(tt.haveInner)
+			if err != nil {
+				t.Fatalf("unexpected error creating store: %v", err)
+			} else if store == nil {
+				t.Fatal("unexpected nil store")
+			}
 
-// 			gotAggregate := tt.haveAggregate
-// 			gotErr := store.Save(context.Background(), gotAggregate, tt.haveOpts)
+			store.BeforeSave(tt.haveHooks[aggregatestore.BeforeSave]...)
+			store.AfterSave(tt.haveHooks[aggregatestore.AfterSave]...)
 
-// 			if tt.wantErr != nil {
-// 				if gotErr == nil || gotErr.Error() != tt.wantErr.Error() {
-// 					t.Errorf("want error: %v, got: %v", tt.wantErr, gotErr)
-// 				}
-// 				return
-// 			}
+			gotAggregate := tt.haveAggregate
+			gotErr := store.Save(context.Background(), gotAggregate, tt.haveOpts)
 
-// 			if gotErr != nil {
-// 				t.Errorf("unexpected error: %v", gotErr)
-// 			} else if gotAggregate == nil {
-// 				t.Errorf("unexpected nil aggregate")
-// 			}
+			if tt.wantErr != nil {
+				if gotErr == nil || gotErr.Error() != tt.wantErr.Error() {
+					t.Errorf("want error: %v, got: %v", tt.wantErr, gotErr)
+				}
+				return
+			}
 
-// 			// aggregate has the correct ID
-// 			if gotAggregate.ID().String() != typeid.FromUUID("mockentity", aggregateID.UUID()).String() {
-// 				t.Errorf("want aggregate ID %s, got %s", typeid.FromUUID("mockentity", aggregateID.UUID()), gotAggregate.ID())
-// 			}
-// 			// aggregate has the correct version
-// 			if gotAggregate.Version() != tt.wantAggregate.Version() {
-// 				t.Errorf("want aggregate version %d, got %d", tt.wantAggregate.Version(), gotAggregate.Version())
-// 			}
-// 		})
-// 	}
-// }
+			if gotErr != nil {
+				t.Errorf("unexpected error: %v", gotErr)
+			} else if gotAggregate == nil {
+				t.Errorf("unexpected nil aggregate")
+			}
+
+			// aggregate has the correct ID
+			if gotAggregate.ID().String() != typeid.FromUUID("mockentity", aggregateID.UUID()).String() {
+				t.Errorf("want aggregate ID %s, got %s", typeid.FromUUID("mockentity", aggregateID.UUID()), gotAggregate.ID())
+			}
+			// aggregate has the correct version
+			if gotAggregate.Version() != tt.wantAggregate.Version() {
+				t.Errorf("want aggregate version %d, got %d", tt.wantAggregate.Version(), gotAggregate.Version())
+			}
+		})
+	}
+}
